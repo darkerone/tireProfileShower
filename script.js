6e75300aee44f487704b8639b9aa13f4255d06de
Antoine Chaupin (UX305) <antoine.chaupin@outlook.fr>
2021-09-28 11:18:42 +0200

    Utilisation canvas pour rectangle description

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let tireManager = new TireManager(allTiresFromData);
 let filterManager = new FilterManager(allTiresFromData)
 
 $(function () {
-  
+
   $("#filters").append(filterManager.getSeasonCheckboxList());
   $("#filters").append(filterManager.getBrandCheckboxList());
 
@@ -56,11 +56,59 @@ function getCarouselItem(tire, isActive){
   let isActiveClass = isActive ? "active" : "";
   let $carouselItem = $("<div class='carousel-item "+ isActiveClass +"'></div>");
   let $itemImage = $("<img src='images/"+ tire.image +"' class='d-block w-100'>")
-  $carouselItem.append($itemImage);
+  //$carouselItem.append($itemImage);
+  $carouselItem.append(getCanvas("images/"+ tire.image, tire.name));
   let $carouselCaption = $("<div class='carousel-caption d-none d-md-block'><h5>"+ tire.name +"</h5></div>");
   $carouselItem.append($carouselCaption);
 
   return $carouselItem;
+}
+
+function getCanvas(imageUrl, title){
+  let width = $(window).width();
+  let height = $(window).width()*4/3;
+
+  let rectLeftRightDistance = 30;
+  let rectBottomDistance = 50;
+  let rectWidth = width - 2 * rectLeftRightDistance;
+  let rectHeight = 90;
+
+  let canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  let ctx = canvas.getContext('2d');
+
+  var imageObj = new Image();
+  imageObj.onload = function() { 
+    ctx.drawImage(imageObj, 0,0, width, height);  
+    ctx.fillStyle = 'black';
+    ctx.fillRect(rectLeftRightDistance, height - rectBottomDistance - rectHeight, rectWidth, rectHeight);
+    //ctx.font = '30px arial';
+    //ctx.fillStyle = 'white';
+    //ctx.fillText(title, rectLeftRightDistance + 10, height - rectBottomDistance - rectHeight + rectHeight/2);
+    drawTextInBox(ctx, title, 'arial', rectLeftRightDistance, height - rectBottomDistance - rectHeight + rectHeight/4, rectWidth, rectHeight/2, 0);
+  }
+  imageObj.src = imageUrl;
+
+  return $(canvas);
+}
+
+function drawTextInBox(ctx, txt, font, x, y, w, h, angle) {
+  angle = angle || 0;
+  let fontHeight = 15;
+  let hMargin = 4;
+  ctx.fillStyle = 'white';
+  ctx.font = fontHeight + 'px ' + font;
+  ctx.textAlign = 'left';
+  ctx.textBaseline = 'top';
+  let txtWidth = ctx.measureText(txt).width + 2 * hMargin;
+  ctx.save();
+  ctx.translate(x+w/2, y);
+  ctx.rotate(angle);
+  ctx.scale(w / txtWidth, h / fontHeight);
+  ctx.translate(hMargin, 0)
+  ctx.fillText(txt, -txtWidth/2, 0);
+  ctx.restore();
 }
 
 function addItemToCarousel($carousel, tire, itemIndex){
